ca5cac8c8bb8dcc13c5adbde96c14b361d80e2c3
Przemysław Buczkowski <przemub@przemub.pl>
2016-10-30 12:19:49 +0100

    rewritten in accordance with PEP 8, minor fixes

--- brita.py
+++ brita.py
@@ -6,152 +6,155 @@
 import gettext
 import locale
 import os
-from sys import argv, exit, platform, stderr
 import sys
 
-def initlocalization():
-	gettext.textdomain("brita")
+def init_localization():
+    gettext.textdomain("brita")
 
-	filename = gettext.find("brita")
-	if filename == None:
-		filename = gettext.find("brita", "build/mo")
+    filename = gettext.find("brita")
+    if filename == None:
+        filename = gettext.find("brita", "build/mo")
 
-	try:
-		if filename != None:
-			trans = gettext.GNUTranslations(open(filename, "rb"))
-		else:
-			trans = gettext.NullTranslations()
-	except IOError:
-		trans = gettext.NullTranslations()
+    try:
+        if filename != None:
+            trans = gettext.GNUTranslations(open(filename, "rb"))
+        else:
+            trans = gettext.NullTranslations()
+    except IOError:
+        trans = gettext.NullTranslations()
 
-	trans.install()
+    trans.install()
 
-def readbrightness(printit = True):
-	global brightness, max_brightness
+def read_brightness(verbose = True):
+    global brightness, max_brightness
 
-	brightness = int(brightnessfile.read().strip())
-	max_brightness = int(max_brightnessfile.read().strip())
+    brightness = int(brightnessfile.read().strip())
+    max_brightness = int(max_brightnessfile.read().strip())
 
-	brightnessfile.seek(0)
-	max_brightnessfile.seek(0)
+    brightnessfile.seek(0)
+    max_brightnessfile.seek(0)
 
-	if not printit:
-		return
+    if not verbose:
+        return
 
-	percent = int(brightness / max_brightness * 100)
-	print(_("%s%%")
-		% (percent))
+    percent = int(brightness / max_brightness * 100)
+    print(_("%s%%") % (percent))
 
-def main(printit = True, lower_bound = False):
-	if platform != 'linux':
-		if printit:
-			stderr.write(_("This script works only in Linux environment.")
-				+ "\r\n")
-		exit(1)
+def main(verbose = True, lower_bound = False):
+    if sys.platform != 'linux':
+        if verbose:
+            sys.stderr.write(_("This script works only in a Linux environment.")
+                    + "\r\n")
+            sys.exit(1)
 
-	directory = "/sys/class/backlight/"
-	device = os.listdir(directory)[0]
-	directory += device + "/"
-	if printit:
-		print(_("Found backlight device: %s") % (device))
+    directory = "/sys/class/backlight/"
+    device = os.listdir(directory)[0]
+    directory += device + "/"
+    if verbose:
+        print(_("Found backlight device: %s") % (device))
 
-	properties = os.listdir(directory)
-	if not 'brightness' in properties or not 'max_brightness' in properties:
-		if printit:
-			stderr.write(_("Brightness settings aren't available for the device.\n"))
-		exit(1)
+    properties = os.listdir(directory)
+    if not 'brightness' in properties or not 'max_brightness' in properties:
+        if verbose:
+            sys.stderr.write(_("Brightness settings aren't available " +
+                "for the device.\n"))
+            sys.exit(1)
 
-	global brightness, max_brightness
-	global brightnessfile, max_brightnessfile
+    global brightness, max_brightness
+    global brightnessfile, max_brightnessfile
 
-	brightnessfile = open(directory + 'brightness', 'r+')
+    brightnessfile = open(directory + 'brightness', 'r+')
 
-	max_brightnessfile = open(directory + 'max_brightness', 'r')
+    max_brightnessfile = open(directory + 'max_brightness', 'r')
 
-	if len(argv) == 1:
-		readbrightness(printit)
-		exit(0)
+    if len(sys.argv) == 1:
+        read_brightness(verbose)
+        exit(0)
 
-	wantedbrightness = argv[-1]
+    wanted_brightness = sys.argv[-1]
 
-	if wantedbrightness[0] == '+' or wantedbrightness[0] == '-':
-		readbrightness(False)
+    if wanted_brightness[0] == '+' or wanted_brightness[0] == '-':
+        read_brightness(False)
 
-		if wantedbrightness[-1] == '%':
-			percent = float(wantedbrightness[1:-1]) / 100
-			if (wantedbrightness[0] == '+'):
-				brightness = int(brightness + max_brightness * percent)
-			elif (wantedbrightness[0] == '-'):
-				brightness = int(brightness - max_brightness * percent)
-		else:
-			if (wantedbrightness[0] == '+'):
-				brightness = brightness + int(wantedbrightness[1:])
-			elif (wantedbrightness[0] == '-'):
-				brightness = brightness - int(wantedbrightness[1:])
+        if wanted_brightness[-1] == '%':
+            percent = float(wanted_brightness[1:-1]) / 100
+            if (wanted_brightness[0] == '+'):
+                brightness = int(brightness + max_brightness * percent)
+            elif (wantedbrightness[0] == '-'):
+                brightness = int(brightness - max_brightness * percent)
+        else:
+            if (wanted_brightness[0] == '+'):
+                brightness = brightness + int(wanted_brightness[1:])
+            elif (wanted_brightness[0] == '-'):
+                brightness = brightness - int(wanted_brightness[1:])
 
-		if brightness > max_brightness:
-			brightness = max_brightness
-		if brightness < 0:
-				if lower_bound:
-						brightness = 100
-				else:
-						brightness = 0
-		if printit:
-			print(brightness)
-		brightnessfile.write(str(brightness))
-		brightnessfile.seek(0)
+        if brightness > max_brightness:
+            brightness = max_brightness
 
-		readbrightness(printit)
-		exit(0)
-	elif wantedbrightness[-1] == '%':
-		readbrightness(False)
+        if brightness < 0:
+            if lower_bound:
+                brightness = 100
+            else:
+                brightness = 0
 
-		percent = float(wantedbrightness[:-1]) / 100
-		brightness = int(float(max_brightness) * percent)
+        if verbose:
+            print(brightness)
+        brightnessfile.write(str(brightness))
+        brightnessfile.seek(0)
 
-		if brightness > max_brightness:
-				brightness = max_brightness
-		if brightness < 0:
-				if lower_bound:
-						brightness = 100
-				else:
-						brightness = 0
+        readbrightness(verbose)
+        exit(0)
+    elif wantedbrightness[-1] == '%':
+        readbrightness(False)
 
-		brightnessfile.write(str(brightness))
-		brightnessfile.seek(0)
+        percent = float(wantedbrightness[:-1]) / 100
+        brightness = int(float(max_brightness) * percent)
 
-		readbrightness(printit)
+        if brightness > max_brightness:
+            brightness = max_brightness
+        if brightness < 0:
+            if lower_bound:
+                brightness = 100
+            else:
+                brightness = 0
+
+        brightnessfile.write(str(brightness))
+        brightnessfile.seek(0)
+
+        read_brightness(verbose)
 
 if __name__ == "__main__":
-	initlocalization()
+    init_localization()
 
-	version = "0.2"
-	synopsis = _("Brita %s - backlight adjuster for Linux") % (version)
+    version = "0.2"
+    synopsis = _("Brita %s - backlight adjuster for Linux") % (version)
 
-	synopsis += "\n" +_('''Usage: brita [options] value
-	Options:
-	  -q, --quiet		Disable printing output
-	  -v, --version		Print current version and exit
-	  -h, --help		Print this message and exit
-	  -m, --min			Set minimum brightness to 100
-	  
-	Value can be in percents (ie. 50%)
-		       or relative to the current value (ie. -500, +30%, -10%)
+    synopsis += "\n" +_('''Usage: brita [options] value
+\tOptions:
+\t  -q, --quiet\tDisable printing output
+\t  -v, --version\tPrint the current version and exit
+\t  -h, --help\tPrint this message and exit
+\t  -m, --min\tDon't set the brightness to a value lower than 100
 
-	Program executed without arguments shows the current brightness''')	
-	
-	State1 = True;
-	State2 = False;
-	
-	for arg in sys.argv:
-		if arg == "-h" or arg == "--help":
-			print(synopsis)
-			exit(0)
-		if arg == "-v" or arg == "--version":
-			print(version)
-			exit(0)
-		if arg == "-q" or arg == "--quiet":
-			State1 = False
-		if arg == "-m" or arg == "--min":
-			State2 = True		
-	main(State1,State2)
+\tValue can be given in percents (ie. 50%)
+\t\tor relative to the current value (ie. -500, +30%, -10%)
+
+\tProgram executed without arguments prints the current brightness.''')
+
+    verbose = True;
+    lower = False;
+
+    for arg in sys.argv:
+        if arg == "-h" or arg == "--help":
+            print(synopsis)
+            exit(0)
+        elif arg == "-v" or arg == "--version":
+            print(version)
+            exit(0)
+        elif arg == "-q" or arg == "--quiet":
+            verbose = False
+        elif arg == "-m" or arg == "--min":
+            lower = True
+
+    main(verbose, lower)
+
